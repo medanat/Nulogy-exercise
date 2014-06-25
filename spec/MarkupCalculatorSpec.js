a850de875fab23ce190dec977669256bf5e31675
Yazan Medanat <medanat@gmail.com>
2014-06-25 13:32:07 -0400

    Added base price and flat markup tests.

--- spec/MarkupCalculatorSpec.js
+++ spec/MarkupCalculatorSpec.js
@@ -13,4 +13,56 @@ describe("MarkupCalculator", function () {
     expect(result).not.toBeNull();
   });
 
+
+  describe("base price", function () {
+
+    it("should not throw an error when given valid base price", function () {
+      var input = {
+            basePrice: -100
+          },
+          calculation = function () {
+            return markupCalculator.calculate(input);
+          };
+
+      expect(calculation).not.toThrowError("invalid base price");
+    });
+
+    it("should throw an error when given invalid base price", function () {
+      var input = {
+            basePrice: -100
+          },
+          calculation = function () {
+            return markupCalculator.calculate(input);
+          };
+
+      expect(calculation).toThrowError("invalid base price");
+    });
+
+    it("should default base price to zero when not given a base price parameter", function () {
+      var input = {},
+          result = markupCalculator.calculate(input);
+
+      expect(result).toEqual(0);
+    });
+  });
+
+
+  describe("flat markup", function () {
+
+    it("should calculate the flat markup correctly when given valid input", function () {
+      var result = markupCalculator.calculateFlatMarkup(100);
+
+      expect(result).toEqual(105);
+    });
+  });
+
+
+  describe("people", function () {
+
+  });
+
+
+  describe("category", function () {
+
+  });
 });
--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -1,7 +1,23 @@
 function MarkupCalculator () {
+  // Predefined flat markup on all jobs
+  this.flatMarkup = 0.05;
 }
 
-
+/**
+ * Calculate cost of a packaging job
+ *   @param {object} options:
+ *     basePrice {float} - defaults to 0
+ *     people {integer} - defaults to 1
+ *     category {string} - optional
+ *
+ */
 MarkupCalculator.prototype.calculate = function (options) {
+  var basePrice = options.basePrice || 0,
+      total = this.calculateFlatMarkup(basePrice);
 
+  return total;
+};
+
+MarkupCalculator.prototype.calculateFlatMarkup = function (basePrice) {
+  return basePrice;
 };
